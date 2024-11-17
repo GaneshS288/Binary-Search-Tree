d1abcf5e9a1d0531337eed9df593df1bb244e487
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2024-11-17 16:04:15 +0530

    create Node and Tree classes. Add buildTree Methods to Tree class to make a binary tree from sorted array

---
+++ bst.js
@@ -0,0 +1,30 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.root = array;
+  }
+
+  removeDuplicates(array) {
+    let setWithoutDuplicate = new Set(array);
+    return Array.from(setWithoutDuplicate);
+  }
+
+  buildTree(array, start = 0, end = array.length - 1) {
+    if (start > end) return null;
+
+    let midpoint = Math.floor((start + end) / 2);
+    let root = new Node(array[midpoint]);
+
+    root.left = this.buildTree(array, start, midpoint - 1);
+    root.right = this.buildTree(array, midpoint + 1, end);
+
+    return root;
+  }
+}
